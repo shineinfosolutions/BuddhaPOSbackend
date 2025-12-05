d6504d0c1c3ffc207a2244e9009bcf7604d5e590
Kritika Singh <kritikasingh273013@gmail.com>
2025-12-05 12:00:03 +0530

    localhost

--- src/server.js
+++ src/server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'http://localhost:3001', 
+    'http://localhost:3001',
+    'http://localhost:5173',
     'https://buddhapos.vercel.app',
     'https://buddha-po-sbackend.vercel.app'
   ],
