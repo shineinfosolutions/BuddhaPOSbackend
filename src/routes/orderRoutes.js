f4219b1926fadf936d513448fe8ef40e03b4dd06
Kritika Singh <kritikasingh273013@gmail.com>
2025-12-05 14:44:57 +0530

    pagination

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -4,6 +4,7 @@ const ctrl = require('../controllers/orderController');
 
 router.post('/', ctrl.createOrder);
 router.get('/', ctrl.getOrders);
+router.get('/search', ctrl.getOrders); // Alias for search
 router.get('/:id', ctrl.getOrderById);
 router.put('/:id', ctrl.updateOrder);
 router.delete('/:id', ctrl.deleteOrder);
